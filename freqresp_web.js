9845261e690f9a909ec9495375e55ed6ac0e415c
shabaz123 <shabaz123@users.noreply.github.com>
2022-04-28 17:56:48 +0100

    Added normalization and auto-file-download
    Added normalization capability, and also Excel-compatible (CSV) auto-file-download upon test completion, and also a dummy mode (for use when there is no hardware attached)

--- freqresp_web.js
+++ freqresp_web.js
@@ -8,12 +8,23 @@ var fs=require('fs');
 var child=require('child_process');
 var execSync = require("child_process").execSync;
 
-var progpath='/home/pi/development/webdsp/';
+var progpath=__dirname+'/';
 var wmpath='/home/pi/development/waveminer/';
 var cmdtext = 'freqresp -a 1.0 -u -m -f ';
 
+var use_dummy=0; 
+var dummy_x = [10,12,14,16,18,20,24,27,30,35,40,45,50,60,70,80,90,100,120,140,160,180,200,240,270,300,350,400,450,500,600,700,800,900,1000,1200,1400,1600,1800,2000,2400,2700,3000,3500,4000,4500,5000,6000,7000,8000,9000,10000,12000,14000,16000,18000,20000];
+var dummy_y = [-0.198883,0.261553,0.71278,0.868271,1.10064,1.260572,1.499121,1.552635,1.668832,1.740032,1.825817,1.847452,1.881889,1.909584,1.933214,1.931055,1.934417,1.946674,1.942369,1.940709,1.945616,1.941839,1.942722,1.933462,1.931905,1.932931,1.929072,1.92801,1.92801,1.926415,1.922799,1.92138,1.918256,1.917156,1.915983,1.911967,1.906631,1.901465,1.896401,1.890831,1.878629,1.869591,1.861001,1.842397,1.819865,1.796083,1.772171,1.71499,1.650253,1.578782,1.498574,1.416777,1.232265,1.032038,0.817606,0.587116,0.332056];
+
 app.listen(8081);
 console.log('freqresp_web.js is running. Go to http://xx.xx.xx.xx:8081/freqresp.html');
+
+if (process.argv.length > 2) {
+    if (process.argv[2] == "dummy") {
+        console.log("using dummy data");
+        use_dummy=1;
+    }
+}
 
 function handler(req, res){
     console.log('url is '+req.url.substr(1));
@@ -39,25 +50,10 @@ io.sockets.on('connection', function(socket){
 
     socket.on('start_test', function (data) {
         console.log('emitting chart_data');
+        cfreqidx = 0;
+        decmult = 10;
         cfreq = freqpoint[cfreqidx] * decmult;
-        prog=child.exec(wmpath+cmdtext+cfreq, function (error, data, stderr) {
-	          value=data.toString();
-            //data = execSync(wmpath+'freqresp -a 1.0 -p -m -f '+i)
-            myData = [cfreq, parseFloat(value)];
-            console.log('emitting '+myData[0]+' '+myData[1]);
-	  		    socket.emit('chart_data', myData);
-            cfreqidx = cfreqidx + 1;
-		    });
-    });
-
-    socket.on('continue_test', function (data) {
-        if ((decmult >= 10000) && (freqpoint[cfreqidx] > 2.0)) {
-            socket.emit('test_conpleted');
-            cfreqidx = 0;
-            decmult = 10;
-        } else {
-            console.log('continue emitting chart_data');
-            cfreq = freqpoint[cfreqidx] * decmult;
+        if (use_dummy==0) {
             prog=child.exec(wmpath+cmdtext+cfreq, function (error, data, stderr) {
 	            value=data.toString();
                 //data = execSync(wmpath+'freqresp -a 1.0 -p -m -f '+i)
@@ -65,11 +61,49 @@ io.sockets.on('connection', function(socket){
                 console.log('emitting '+myData[0]+' '+myData[1]);
 	  		    socket.emit('chart_data', myData);
                 cfreqidx = cfreqidx + 1;
+		    });
+        } else {
+            // use dummy data
+            myData = [dummy_x[cfreqidx], dummy_y[cfreqidx]];
+            console.log('emitting dummy '+myData[0]+' '+myData[1]);
+            socket.emit('chart_data', myData);
+            cfreqidx = cfreqidx + 1;
+        }
+    });
+
+    socket.on('continue_test', function (data) {
+        if ((decmult >= 10000) && (freqpoint[cfreqidx] > 2.0)) {
+            socket.emit('data_complete');
+            cfreqidx = 0;
+            decmult = 10;
+        } else {
+            console.log('continue emitting chart_data');
+            cfreq = freqpoint[cfreqidx] * decmult;
+            if (use_dummy==0) {
+                prog=child.exec(wmpath+cmdtext+cfreq, function (error, data, stderr) {
+	                value=data.toString();
+                    //data = execSync(wmpath+'freqresp -a 1.0 -p -m -f '+i)
+                    myData = [cfreq, parseFloat(value)];
+                    console.log('emitting '+myData[0]+' '+myData[1]);
+	  		        socket.emit('chart_data', myData);
+                    cfreqidx = cfreqidx + 1;
+                    if (cfreqidx >= numfp) {
+                        cfreqidx = 0;
+                        decmult = decmult * 10;
+                    }
+		        });
+            } else {
+                // use dummy data
+                var dummyidx = dummy_x.findIndex((element) => element == cfreq);
+                myData = [dummy_x[dummyidx], dummy_y[dummyidx]+(Math.random()/10)];
+                console.log('emitting dummy '+myData[0]+' '+myData[1]);
+                socket.emit('chart_data', myData);
+                cfreqidx = cfreqidx + 1;
                 if (cfreqidx >= numfp) {
                     cfreqidx = 0;
                     decmult = decmult * 10;
                 }
-		    });
+            }
         }
     });
 
--- scripts.js
+++ scripts.js
@@ -2,28 +2,56 @@
 
 var socket; // used for socket.io
 
-var trace1 = { 
-    x: [],
-    y:[],
-    type: 'scatter'
-};
+// trace1 contains the actual data points
+var trace1;
+// data contains all trace arrays
+var data;
+// norm1 contains a copy of trace1 after normalization
+var norm1;
+var norm_saved = 0;
+var idx;
 
-var data = [trace1];
+// execution states
+const IDLE = 0;
+const DO_NORM = 1;
+const DO_TEST = 2;
+
+// button states
+const BTN_START_TEST = 0;
+const BTN_STOP_TEST = 1;
+const BTN_RUN_NORM = 0;
+const BTN_UN_NORM = 1;
+const BTN_NORM_IN_PROGRESS = 2;
+
+var state = IDLE;
+var btn_test_state = BTN_START_TEST;
+var btn_norm_state = BTN_RUN_NORM;
+var abort_test = 0;
 
 var layout = {
     xaxis: {
         title: 'Frequency (Hz)',
         type: 'log',
-        autorange: true,
+        autorange: false,
+        range: [1, 5] /* 10^1 (10 Hz) to 10^5 (100 kHz) */
     },
     yaxis: {
         title: 'Amplitude (dB)',
         autorange: true
     },
-    title: 'Frequency Response'
+    title: 'Frequency Response',
+    line: {
+        color: 'rgb(0,0,0)' /* black */
+    }
 };
 
-var frchart_created=0;
+var norm_trace1_update = {
+    line: {
+        color: 'rgb(210,210,210)' /* light gray */
+    }
+};
+
+var norm_data_update = [norm_trace1_update];
 
 
 $(document).ready(function() {
@@ -32,37 +60,153 @@ $(document).ready(function() {
 
 jQuery(document).ready(function() {
 
+    Plotly.newPlot('frchart_div', data, layout);
 	
-	$("#id_status").html('Connecting to Instrument..');
-
+	$("#id_status").html('Status: Ready');
 	
-	$("#id_start").click(function(e){
-        do_start();
+	$("#id_test").click(function(e){
+        if (btn_test_state==BTN_START_TEST) {
+            do_start();
+        } else {
+            do_stop();
+        }
 	});
-	$("#id_stop").click(function(e){
-        do_stop();
+    $("#id_norm").click(function(e){
+        if (btn_norm_state==BTN_RUN_NORM) {
+            do_norm();
+        } else if (btn_norm_state==BTN_UN_NORM){
+            do_unnorm();
+        } else {
+            // do nothing, normalization is in progress
+        }
 	});
 
     socket = io.connect();
 
-    socket.on('chart_data', function(data) {
-        $("#id_status").html(data[1].toFixed(2) + ' dB at '+data[0]+' Hz');
-        Plotly.extendTraces('frchart_div', {x: [[data[0]]], y: [[data[1]]]}, [0]);
-        socket.emit('continue_test');   
+    socket.on('chart_data', function(d) {
+        xdatapoint = d[0];
+        ydatapoint = d[1];
+        if (state == DO_TEST) {
+            if (norm_saved) {
+                idx = norm1.x.findIndex((element) => element == xdatapoint);
+                if (idx>=0) {
+                    ydatapoint = ydatapoint - norm1.y[idx];
+                } else {
+                    console.log('error, normalization indices do not match the test data!');
+                }
+            }
+        }
+        $("#id_status").html(ydatapoint.toFixed(2) + ' dB at '+xdatapoint+' Hz');
+        Plotly.extendTraces('frchart_div', {x: [[xdatapoint]], y: [[ydatapoint]]}, [0]);
+        if (abort_test==1) { // don't request any more data points, reset the button
+            $("#id_status").html('Test aborted');
+            abort_test=0;
+        } else { // request the next data point
+            socket.emit('continue_test');   
+        }
+    });
+
+    socket.on('data_complete', function(d) {
+        if (state==DO_NORM) {
+            // normalization completed
+            norm1 = trace1; // store the normalization values
+            norm_saved = 1;
+            $("#id_status").html('Normalization Saved');
+            btn_norm_state = BTN_UN_NORM;
+            $("#id_norm").html("Undo Normalization").attr("disabled", false);
+        } else if (state==DO_TEST) {
+            // test completed
+            if (norm_saved==1) {
+                $("#id_status").html('Test complete (with normalization applied)');
+            } else {
+                $("#id_status").html('Test complete (no normalization applied)');
+            }
+            $("#id_test").html("Start Test").removeClass('btn-danger').addClass('btn-success');
+            transfer_dataset(trace1);
+            btn_test_state=BTN_START_TEST;
+        }
+        state = IDLE;
     });
     
 })
 
-function do_start(){
-    $("#id_status").html('Test Started ');
-    if (frchart_created==0) {
-        Plotly.newPlot('frchart_div', data, layout);
-        frchart_created = 1;
+// download function from https://stackoverflow.com/questions/13405129/create-and-save-a-file-with-javascript
+function download(data, filename, type) {
+    var file = new Blob([data], {type: type});
+    if (window.navigator.msSaveOrOpenBlob) // IE10+
+        window.navigator.msSaveOrOpenBlob(file, filename);
+    else { // Others
+        var a = document.createElement("a"),
+                url = URL.createObjectURL(file);
+        a.href = url;
+        a.download = filename;
+        document.body.appendChild(a);
+        a.click();
+        setTimeout(function() {
+            document.body.removeChild(a);
+            window.URL.revokeObjectURL(url);  
+        }, 0); 
+    }
+}
+
+function transfer_dataset(tracedata) {
+    var filetext = "";
+    var i;
+    for (i=0; i<tracedata.x.length; i++) {
+        filetext = filetext.concat(tracedata.x[i]+','+tracedata.y[i]+'\n');
+    }
+    download(filetext, 'freq_resp.csv', 'text/plain');
+}
+
+// called when running normalization or test
+function start_data() {
+    // create an empty set of data points
+    trace1 = { 
+        x: [],
+        y:[],
+        type: 'scatter'
+    };
+    data = [trace1];
+
+    Plotly.react('frchart_div', data, layout); // erases any existing chart
+    if (state==DO_NORM) {
+        // switch trace color if we are doing normalization:
+        Plotly.restyle('frchart_div', norm_trace1_update);
     }
     socket.emit('start_test');
+}
+
+// start the test
+function do_start(){
+    $("#id_status").html('Running Test ');
+    abort_test = 0;
+    state = DO_TEST;
+    btn_test_state = BTN_STOP_TEST;
+    $("#id_test").html("Stop Test").removeClass('btn-success').addClass('btn-danger');
+    start_data(); // kick off the data gathering and chart plotting for the test
 };
 
 function do_stop(){
     $("#id_status").html('Test Stopped ');
+    abort_test = 1;
+    btn_test_state = BTN_START_TEST;
+    $("#id_test").html("Start Test").removeClass('btn-danger').addClass('btn-success');
+};
+
+// start the normalization
+function do_norm(){
+    $("#id_status").html('Running Normalization ');
+    state = DO_NORM;
+    norm_saved = 0; // forget any previous normalization
+    btn_norm_state = BTN_NORM_IN_PROGRESS;
+    $("#id_norm").html("Running Normalization..").removeClass('btn-success').addClass('btn-secondary').attr("disabled", true);
+    start_data(); // kick off the data gathering and chart plotting for the normalization
+};
+
+function do_unnorm(){
+    $("#id_status").html('Normalization Erased ');
+    norm_saved = 0; // forget any previous normalization
+    btn_norm_state = BTN_RUN_NORM;
+    $("#id_norm").html("Run Normalize").removeClass('btn-secondary').addClass('btn-success');
 };
 
